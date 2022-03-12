f0b7114a178af07d8e1fd23ebb985e9cf16d7f4c
Dominic Nguyen <35666615+datduyng@users.noreply.github.com>
2022-03-12 21:03:02 +0000

    placeholder

---
+++ app/components/ResourceCard/ResourceCard.tsx
@@ -0,0 +1,12 @@
+type Props = {
+
+}
+
+const ResourceCard = () => {
+
+  return (
+    <>
+    </>
+  );
+};
+export default ResourceCard;
---
+++ app/components/ResourceCard/index.ts
@@ -0,0 +1,1 @@
+export { default as ResourceCard } from './ResourceCard'
